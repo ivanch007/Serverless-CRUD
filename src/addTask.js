9f88f344d53ad9fd45ab0c81c2baec3c13b269ce
Ivan Ocampo <ivanocampo07@gmail.com>
2025-01-09 16:36:20 -0500

    the lambda addTask is created

---
+++ src/addTask.js
@@ -0,0 +1,30 @@
+const {v4} = require ('uuid')
+const AWS = require('aws-sdk')
+
+const addTask = async(event) =>{
+    const dynamoDB = new AWS.DynamoDB.DocumentClient();
+    const {title, description} = JSON.parse(event.body);
+    const createdAt = new Date().toISOString();
+    const id = v4();
+
+    const newTask = {
+        id,
+        title,
+        description,
+        createdAt
+    };
+
+    await dynamoDB.put({
+        TableName: 'TasksTable',
+        Item: newTask
+    }).promise();
+
+    return {
+        statusCode: 200,
+        body:JSON.stringify(newTask)
+    }
+}
+
+module.exports = {
+    addTask
+};
